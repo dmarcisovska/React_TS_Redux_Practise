5b5003e371163a8b7f99e210692f7e2553dca039
Denisa Marcisovska <dmarcisovska@gmail.com>
2026-02-07 14:56:31 -0800

    redux updates

--- react_ts_redux/src/App.tsx
+++ react_ts_redux/src/App.tsx
@@ -1,8 +1,29 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import Counter from "./Counter";
+import Counter from "./components/Counter";
 import "./App.css";
+
+// Store 
+interface CounterState {
+  count: number;
+
+}
+
+
+
+interface userState {
+  isSignedIn: boolean;
+}
+
+// Actions 
+
+const increment = {type: "INCREMENT"} 
+const decrement = {type: "DECREMENT"} 
+
+// Reducers 
+
+
 
 function App() {
   const [count, setCount] = useState(0);
---
+++ react_ts_redux/src/state/counter/counterSlice.ts
@@ -0,0 +1,19 @@
+import {create}
+
+interface CounterState {
+    value: number;
+
+}
+
+const initialState: CounterState = {
+    value: 0,
+};
+
+const counterSlice = createSlice({
+    name: "counter",
+    initialState,
+    reducers: {},
+
+});
+
+export default counterSlice.reducer;
