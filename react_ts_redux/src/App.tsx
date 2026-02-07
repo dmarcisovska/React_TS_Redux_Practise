4c41c9570b96b8d9d91e3d189c393a8b41a4b4da
Denisa Marcisovska <dmarcisovska@gmail.com>
2026-02-07 10:49:53 -0800

    formatting

--- react_ts_redux/src/App.tsx
+++ react_ts_redux/src/App.tsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Counter from './Counter'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Counter from "./Counter";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <Counter/>
+      <Counter />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
